b960d9d1381a8b564c25cbaa8319d12615170bd8
bkleinb78 <y2bk728@aol.com>
2023-03-28 22:11:01 -0500

    created the cardlist.js file and made some edits

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
     return (
-       <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-s'>
-         <img alt='robots' src='https://robohash.org/test?200x200'/>
-         <div>
-            <h2>Jane Doe</h2>
-            <p>jane.doe@gmail.com</p>
-         </div>
-       </div> 
+      <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+        <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+        <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+        </div>
+      </div>
     );
-}
-
-export default Card;
+  }
+    
+  export default Card;
